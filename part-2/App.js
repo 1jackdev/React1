4bd2d4a1d7f9b32e09766f7f4f310965fe829d35
Jack <jackmayer21@gmail.com>
2021-03-17 18:48:22 -0700

    making part 2 prettier with bootstrap

--- part-2/App.js
+++ part-2/App.js
@@ -1,24 +1,7 @@
 const App = () => {
   return (
-    <div>
-      <Tweet
-        name="John Smith"
-        username="jsmithy"
-        date="03-17-2021"
-        message="man, aint react the best?"
-      />
-      <Tweet
-        name="Henry Ford"
-        username="thefordguy"
-        date="03-17-1895"
-        message="making cars is easy..."
-      />
-      <Tweet
-        name="Nikola Tesla"
-        username="therealtesla"
-        date="03-17-1895"
-        message="electricity is boring change my mind"
-      />
+    <div className="container-fluid col-9">
+        <Tweet />
     </div>
   );
 };
--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,7 +1,37 @@
 const Tweet = (props) => {
-  return <div>
-      <h2>{props.username} <span>({props.name})</span></h2>
-      <p>{props.message}</p>
-      <p>{props.date}</p>
-  </div>
+  const tweets = [
+    {
+      username: "jsmithy",
+      name: "John Smith",
+      message: "man, aint react the best?",
+      date: "03-17-2021",
+    },
+    {
+      username: "thefordguy",
+      name: "Henry Ford",
+      message: "makings cars is easy...",
+      date: "03-16-1903",
+    },
+    {
+      username: "therealtesla",
+      name: "Nikola Tesla",
+      message: "electricity is so boring.",
+      date: "03-15-1896",
+    },
+  ];
+  const tweetlist = [];
+  for (let tweet of tweets) {
+    tweetlist.push(
+      <div className="card col-sm">
+        <div className="card-body">
+          <h3 className="card-title">
+            {tweet.username} <span>({tweet.name})</span>
+          </h3>
+          <p className="card-text">{tweet.message}</p>
+          <p>{tweet.date}</p>
+        </div>
+      </div>
+    );
+  }
+  return <div className="row">{tweetlist}</div>;
 };
